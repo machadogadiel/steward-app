2be1c17aecefaefa7ffc6ba2302438ae1188b105
Gadiel Rangel <github@gadiel.dev>
2023-12-03 18:37:40 +0000

    use brotli for build files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { compression } from "vite-plugin-compression2";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      compression(),
+      compression({
+        algorithm: "brotliCompress",
+        exclude: [/\.(br)$/, /\.(gz)$/],
+        deleteOriginalAssets: false,
+      }),
+    ],
+    vue: {
+      customElement: true,
+    },
+    vueJsx: {
+      mergeProps: true,
+    },
+  },
+});
